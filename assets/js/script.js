7753623373cbeb27632e59ea8c2d40854f657f7f
Mateus Satoh <mateussatoh@gmail.com>
2020-11-21 09:13:08 -0300

    ⛈  Fix HTTPS request from favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
     let weatherIcon = document.querySelector('.weather-icon')
   
     let body = document.getElementById('body')
-    let pageIcon = document.querySelector("link[rel='icon']")
+  
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -42,7 +42,7 @@ window.addEventListener('load', () => {
                 temperatureDegree.textContent = String(temperatureCelcius) + "°C" 
                 footerDescription.textContent = description
                 weatherIcon.src = iconURL
-                pageIcon.href = iconURL
+                
                 body.style.backgroundImage = `url('./assets/img/${icon}.gif')`;
                 console.log(body.style.backgroundImage)
                 
