03b09ed8e45a7f894678e48b975b9eca917442b4
Mateus Satoh <mateussatoh@gmail.com>
2020-11-21 12:39:56 -0300

    Fix script

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,8 +30,8 @@ window.addEventListener("load", () => {
           const country = data.sys.country;
           const temperatureCelcius = Math.round(data.main.temp);
           const description = data.weather[0].description;
-          //const icon = data.weather[0].icon;
-          const icon = '09d';
+          const icon = data.weather[0].icon;
+         
           const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
           const weatherDataColor = {
